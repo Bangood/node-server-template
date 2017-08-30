e06025efeddd15724adc1992c5ebb4626d379e0b
Pure <2419281635@qq.com>
2017-08-30 10:54:51 +0800

    初始化express

---
+++ config/express.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pure on 2017/8/30.
+ */
+import express from 'express';
+
+const app = express();
+export default app;
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 /**
  * Created by pure on 2017/8/29.
  */
+import app from './config/express';
+app.listen(3001, ()=>{
+  console.log(`server started on port ${3001} (${'development'})`);
+});
