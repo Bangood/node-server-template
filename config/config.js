7543212a3a9d349fed7072ca0e9caf73fff72621
Pure <2419281635@qq.com>
2017-08-30 16:35:13 +0800

    对.env中的环境变量进行验证

--- config/config.js
+++ config/config.js
@@ -2,10 +2,24 @@
  * Created by pure on 2017/8/30.
  */
 import {config as dotenvConfig} from 'dotenv';
+import Joi from 'joi';
 // configure dotenv, will load vars in .env file into process.env
 dotenvConfig();
+// define validation for all the env vars.
+const envVarsSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid(['development', 'production'])
+    .default('development'),
+  PORT: Joi.number()
+    .default(4040)
+}).unknown();
+const {error, value: envVars} = Joi.validate({}, envVarsSchema);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+console.log(typeof envVars.PORT);
 const config = {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT
+  env: envVars.NODE_ENV,
+  port: envVars.PORT
 };
 export default config;
