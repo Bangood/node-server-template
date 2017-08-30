818a6aff51c17decc3158acb2751a566c5e37d90
Pure <2419281635@qq.com>
2017-08-30 14:48:38 +0800

    增加eslint工具

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'extends': 'standard',
+  'rules': {
+    'semi': ['error', 'always'], //强制语句分号结尾
+    'space-before-function-paren': [2, {'anonymous': 'always', 'named': 'never'}], //函数定义时括号前的空格
+    'indent': ['error', 2]
+  }
+};
