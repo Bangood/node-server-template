99af9c87e99a07886a34ead38b66666b8bafbf0c
Pure <2419281635@qq.com>
2017-08-30 11:05:50 +0800

    添加cookie-parser中间件，解析cookie头，并填充req.cookies

--- config/express.js
+++ config/express.js
@@ -3,9 +3,12 @@
  */
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import cookieParser from 'cookie-parser';
 const app = express();
 
+// Parse HTTP request body.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// Parse cookie header and populate req.cookies.
+app.use(cookieParser());
 export default app;
