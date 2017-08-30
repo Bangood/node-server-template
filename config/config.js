93eb0312dec1f493ab71b96508e09b9eb8638a7b
Pure <2419281635@qq.com>
2017-08-30 15:40:20 +0800

    增加对dotenv的注释

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@
  * Created by pure on 2017/8/30.
  */
 import {config as dotenvConfig} from 'dotenv';
+// configure dotenv, will load vars in .env file into process.env
 dotenvConfig();
 const config = {
   env: process.env.NODE_ENV,
